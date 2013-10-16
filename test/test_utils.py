14c606f88f1e9cab23c5014e5827d054d12d6076
Daniel Lombraña González <teleyinex@gmail.com>
2013-10-16 11:22:45 +0200

    Unit test for handle_error

--- test/test_utils.py
+++ test/test_utils.py
@@ -15,6 +15,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import json
 from base import Test, db
 from links import utils
 
@@ -34,7 +35,7 @@ class TestUtils(Test):
             assert utils.valid_link_url(u[0]) == u[1], err_msg
 
     def test_allow_post(self):
-        """Test allow_post_method"""
+        """Test allow_post method"""
         ip = "127.0.0.1"
         # Test first MAX_HITS
         for i in range(0, 5):
@@ -54,3 +55,19 @@ class TestUtils(Test):
         for i in range(0, 5):
             err_msg = "%s st POST should NOT be allowed" % i
             assert utils.allow_post(db, ip, hour=10, max_hits=5) is False, err_msg
+
+    def test_handle_error(self):
+        """Test handle_error method"""
+        errors = [('invalid_url', 'Invalid URL', 415),
+                  ('url_missing', 'url arg is missing', 415),
+                  ('too_many_args', 'Too many arguments. url and project_slug are the only allowed arguments', 415),
+                  ('rate_limit', 'Rate limit reached', 415),
+                  ('project_slug_missing', 'Project slug arg is missing', 415),
+                  ('project_not_found', 'Project not found', 404),
+                  ('server_error', 'Server Error', 500)]
+        for e in errors:
+            res = utils.handle_error(e[0])
+            err = json.loads(res.response[0])
+            assert err['status'] == 'failed', 'Member status != failed'
+            assert err['error'] == e[1], "Wrong error msg for %s: %s" % (e[0],
+                                                                         err['error'])
