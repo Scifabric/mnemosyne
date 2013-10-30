712d7c3c23a22c201114006ce8fd3296576a531b
Daniel Lombraña González <teleyinex@gmail.com>
2013-10-30 11:37:08 +0100

    Rename to mnemosyne

--- mnemosyne/core.py
+++ mnemosyne/core.py
@@ -1,20 +1,20 @@
 # -*- coding: utf8 -*-
-# This file is part of PyBossa-links.
+# This file is part of Mnemosyne.
 #
 # Copyright (C) 2013 Daniel Lombraña González
 #
-# PyBossa-links is free software: you can redistribute it and/or modify
+# Mnemosyne is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# PyBossa-links is distributed in the hope that it will be useful,
+# Mnemosyne is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU Affero General Public License for more details.
 #
 # You should have received a copy of the GNU Affero General Public License
-# along with PyBossa-links. If not, see <http://www.gnu.org/licenses/>.
+# along with Mnemosyne. If not, see <http://www.gnu.org/licenses/>.
 from flask import Flask
 from mnemosyne.frontend import frontend
 from mnemosyne.model import db
