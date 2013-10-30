d3a7656696785218389103c344dd08a6ac7f0208
Daniel Lombraña González <teleyinex@gmail.com>
2013-10-30 11:40:24 +0100

    Update license

--- test/test_app.py
+++ test/test_app.py
@@ -1,20 +1,20 @@
 # -*- coding: utf8 -*-
-# This file is part of PyBossa-links.
+# This file is part of Mnemosyne.
 #
 # Copyright (C) 2013 Daniel Lombraña González
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
+# Mnemosyne is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful,
+# Mnemosyne is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU Affero General Public License
+# along with Mnemosyne. If not, see <http://www.gnu.org/licenses/>.
 import json
 from base import Test
 from mnemosyne.model.link import Link
