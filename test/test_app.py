58f599c43ed34a358868ca9677b7a165dfbd20ec
Daniel Lombraña González <teleyinex@gmail.com>
2013-10-31 13:16:41 +0100

    PEP8 and Docstrings

--- test/test_app.py
+++ test/test_app.py
@@ -15,6 +15,13 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with Mnemosyne. If not, see <http://www.gnu.org/licenses/>.
+"""
+Tests for Link logic and model.
+
+This exports:
+    - TestLinks class for testing Mnemosyne Link object and logic
+
+"""
 import json
 from base import Test
 from mnemosyne.model.link import Link
@@ -22,12 +29,14 @@ from mnemosyne.model.project import Project
 
 
 class TestLinks(Test):
+
+    """Class for testing Link logic and objects."""
+
     def test_00_GET_index(self):
-        """Test GET INDEX web page"""
+        """Test GET INDEX web page."""
         # With nothing on the DB
         res = self.tc.get('/')
         msg = "There are 0 stored URLs and 0 projects"
-        print res.data
         assert msg == res.data, "There should be 0 stored URLs and projects"
 
         # With some links and projects
@@ -36,17 +45,19 @@ class TestLinks(Test):
             n_projects = Project.query.count()
             n_links = Link.query.count()
             res = self.tc.get('/')
-            msg = "There are %s stored URLs and %s projects" % (n_links, n_projects)
-            err_msg = "There should be %s stored URLs and %s projects" % (n_links,
-                                                                          n_projects)
+            msg = "There are %s stored URLs and %s projects" % (n_links,
+                                                                n_projects)
+            err_msg = ("There should be %s stored URLs and %s projects"
+                       % (n_links, n_projects))
             assert msg == res.data, err_msg
 
     def test_POST_index(self):
-        """Test POST INDEX web page"""
+        """Test POST INDEX web page."""
         with self.app.app_context():
             self.fixtures()
             project = Project.query.first()
-            link = {'url': 'http://new.com/img.jpg', 'project_slug': project.slug}
+            link = {'url': 'http://new.com/img.jpg',
+                    'project_slug': project.slug}
             res = self.tc.post('/', data=link)
             status = json.loads(res.data)
             assert status['status'] == 'saved', status
@@ -61,7 +72,7 @@ class TestLinks(Test):
             assert status['id'] is not None, status
 
     def test_GET_project(self):
-        """Test GET PROJECT web page"""
+        """Test GET PROJECT web page."""
         # Empty DB
         with self.app.app_context():
             res = self.tc.get('/project/')
@@ -81,7 +92,8 @@ class TestLinks(Test):
             # Get a specific project
             res = self.tc.get('/project/?slug=%s' % projects[0].slug)
             output = json.loads(res.data)
-            assert output["id"] == projects[0].id, "The returned project is wrong"
+            err_msg = "The returned project is wrong"
+            assert output["id"] == projects[0].id, err_msg
             assert res.status_code == 200, self.ERR_MSG_200_STATUS_CODE
 
             # Get a project that does not exist
