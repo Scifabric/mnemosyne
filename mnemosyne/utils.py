801b44fd8202d4d72e46bf17ba9e747089a4b9e2
Daniel Lombraña González <teleyinex@gmail.com>
2013-12-10 15:48:59 +0100

    util package for crossdomain requests

---
+++ mnemosyne/utils.py
@@ -0,0 +1,85 @@
+# -*- coding: utf8 -*-
+# This file is part of Mnemosyne.
+#
+# Copyright (C) 2013 Daniel Lombraña González
+#
+# Mnemosyne is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Mnemosyne is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Mnemosyne. If not, see <http://www.gnu.org/licenses/>.
+"""
+Utils package for Mnemosyne.
+
+This exports:
+    - crossdomain a decorator to enable crossdomain requests
+
+"""
+from datetime import timedelta
+from flask import current_app, request, make_response
+from functools import update_wrapper
+
+
+# Based on http://flask.pocoo.org/snippets/56/
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):    # pragma: no cover
+    """
+    Enable crossdomain requests.
+
+    Keyword arguments:
+        origin -- Which origin is allowed
+        method -- Which methods are allowed
+        headers -- Wich headers to include
+        max_age -- By default 21600
+        attach_to_all -- By default True
+        automatic_options -- By default True
+
+    Return value:
+        decorator -- Python decorator
+
+    """
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
