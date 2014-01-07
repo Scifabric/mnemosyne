1fc25e526672e9d7d0eb989c289bd7553959bbd8
Daniel Lombraña González <teleyinex@gmail.com>
2014-01-07 18:01:40 +0100

    Fix old name

--- setup.py
+++ setup.py
@@ -5,18 +5,18 @@ except:
     requirements = []
 
 setup(
-    name='pybossa-links',
+    name='mnemosyne',
     version='0.0.1',
     packages=find_packages(),
     install_requires=requirements,
     author='Daniel Lombraña González',
     author_email='teleyinex@gmail.com',
     description='Web module for storing image links',
-    long_description='''PyBossa-links is a web service that saves image links,
+    long_description='''Mnemosyne is a web service that saves image links,
                         analyze them, and create tasks in a PyBossa server''',
     license='AGPLv3',
-    url='https://github.com/teleyinex/pybossa-links',
-    download_url='https://github.com/teleyinex/pybossa-links',
+    url='https://github.com/PyBossa/mnemosyne',
+    download_url='https://github.com/PyBossa/mnemosyne',
     include_package_data=True,
     classifiers=[
         'Development Status :: 3 - Alpha',
